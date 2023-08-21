c45ccc8c20492035ac12e99ccd02f0aef552cbc9
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2023-08-21 13:31:44 +0200

    Added a repository that can Register a User

---
+++ App/Application/Features/Authentication/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using FluentResults;
+
+namespace Application.Features.Authentication.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<Result> Register(User user);
+    }
+}
---
+++ App/Application/Features/Authentication/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using Application.Features.Authentication.Interfaces;
+using Dapper;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Authentication
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        public UserRepository(IConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public async Task<Result> Register(User user)
+        {
+            using IDbConnection connection = _connectionFactory.GetConnection(ConnectionType.SqlConnection);
+            connection.Open();
+
+            int numberOfAffectedRows = await connection.ExecuteAsync(
+                """
+                INSERT INTO Users
+                (UserName, Email, PasswordHash)
+                VALUES
+                (@UserName, @Email, @PasswordHash)
+                """,
+                new
+                {
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    PasswordHash = user.PasswordHash
+                });
+
+            if (numberOfAffectedRows == 1)
+            {
+                return Result.Ok();
+            }
+            return Result.Fail($"{numberOfAffectedRows} rows affected");
+        }
+
+    }
+}
