4e20b7cb98c91ff982b9727ad2c5092c43d3ba58
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2023-09-12 16:47:58 +0200

    Added a dto for conversation creation input data alongside its validator

---
+++ App/Application/Features/Chat/Dtos/ConversationCreationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Chat.Dtos
+{
+    public class ConversationCreationDto
+    {
+        public List<int>? ConversationMemberIds { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ App/Application/Features/Chat/Validators/ConversationCreationDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Chat.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Chat.Validators
+{
+    public class ConversationCreationDtoValidator : AbstractValidator<ConversationCreationDto>
+    {
+        public ConversationCreationDtoValidator()
+        {
+            RuleFor(p => p.Title)
+                .MustBeValidConversationTitle();
+
+            RuleFor(p => p.Description)
+                .MustBeValidConversationDescription();
+        }
+    }
+}
