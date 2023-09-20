e6cd1ce6640ac53aee2230f3f18bcc10a56779a9
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2023-09-20 12:55:06 +0200

    Added a base type for DomainIds that will be implemented in the future

---
+++ App/Application/Features/Shared/DomainId.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Shared
+{
+    public abstract class DomainId : ValueObject
+    {
+        public DomainId(Guid value)
+        {
+            Value = value;
+        }
+
+        public Guid Value { get; init; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
